5d32552cb2b202746c040a60c97cf4c78de07955
abdul-nut <suryayasaantariksa@gmail.com>
2024-06-13 22:14:08 +0800

    Melanjutkan Saklar

--- semester 1/sesi 4/script.js
+++ semester 1/sesi 4/script.js
@@ -1,35 +1,55 @@
-var lampu1 = document.getElementById("lampu1");
-var lampu2 = document.getElementById("lampu2");
-var lampu3 = document.getElementById("lampu3");
+let lampu1 = document.getElementById("lampu1");
+let lampu2 = document.getElementById("lampu2");
+let lampu3 = document.getElementById("lampu3");
+let toggle = document.getElementById("default-toggle");
 
-var lampuMenyala = {
+let lampuMenyala = {
     lampu1: false,
     lampu2: false,
     lampu3: false
 };
 
+let ruangLampuMenyala = {
+    ruang1: false,
+    ruang2: false
+}
+
+function saklarRuang(ruangId) {
+    let ruangMap = {
+        1: { ruang: 1, ruangKey: "keluarga" }
+    }
+
+    let ruangData = ruangMap[ruangId]
+
+    if (ruangData) {
+        saklar(1)
+        saklar(2)
+        saklar(3)
+    }
+}
+
 function saklar(lampuId) {
-    var lampuMap = {
+    let lampuMap = {
         1: { lampu: lampu1, lampuKey: "lampu1" },
         2: { lampu: lampu2, lampuKey: "lampu2" },
         3: { lampu: lampu3, lampuKey: "lampu3" }
     }
 
-    var lampuData = lampuMap[lampuId]
+    let checked = toggle.checked
+    let lampuData = lampuMap[lampuId]
 
     if (lampuData) {
-        saklarLogic(lampuData.lampu, lampuData.lampuKey);
+        saklarLogic(lampuData.lampu, lampuData.lampuKey, checked);
     }
 
 
 }
 
-function saklarLogic(lampu, lampuKey) {
-    if (lampu.src.includes("off.gif") && !lampuMenyala[lampuKey]) {
-        lampuMenyala[lampuKey] = true;
+function saklarLogic(lampu, lampuKey, checked) {
+    checked = true
+    if (lampu.src.includes("off.gif") && !lampuMenyala[lampuKey] && checked == true) {
         lampu.src = "assets/images/on.gif";
-    } else if (lampuMenyala[lampuKey] === true) {
-        lampuMenyala[lampuKey] = false;
+    } else {
         lampu.src = "assets/images/off.gif";
     }
     lampu.replaceWith(lampu);
