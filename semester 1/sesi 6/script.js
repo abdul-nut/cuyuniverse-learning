20ec899bda8a2299ff160da0d899d71b9982f704
abdul-nut <suryayasaantariksa@gmail.com>
2024-07-02 15:45:35 +0800

    menyelesaikan sesi 6

---
+++ semester 1/sesi 6/script.js
@@ -0,0 +1,25 @@
+let body = document.body
+const darkLimit = 5;
+let darkCounter = 0;
+let darkRemainingLeft;
+let info = document.getElementById('info')
+let modeButton = document.getElementById('modeButton')
+let homeButton = document.getElementById('homeButton')
+
+function switchMode() {
+    if (darkRemainingLeft == 1) {
+        info.textContent = `fitur dark mode sudah habis`
+        modeButton.style.display = 'none'
+        homeButton.textContent = 'RESTART'
+        return;
+    }
+    darkCounter += 1
+    darkRemainingLeft = darkLimit - darkCounter;
+    // Render ke info element
+    info.textContent = `Mengganti Mode Maximal 4 kali dan sudah terpakai ${darkCounter}x`
+    body.classList.toggle("dark")
+}
+
+function reRender(){
+    location.reload();
+}
