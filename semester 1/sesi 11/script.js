5851a99e85ba53a9d8a74d065826a87a24e87736
abdul-nut <suryayasaantariksa@gmail.com>
2024-07-09 21:28:18 +0800

    menyelesaikan sesi 11

---
+++ semester 1/sesi 11/script.js
@@ -0,0 +1,8 @@
+const circles = document.querySelectorAll('.circle')
+
+circles.forEach(circle => {
+    circle.addEventListener('click', ({ target }) => {
+        const { backgroundColor } = getComputedStyle(target)
+        document.body.style.backgroundColor = backgroundColor;
+    })
+});
